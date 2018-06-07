d5879512e706f831cfc55f36c91a4f9280ccd6f0
Alex <xandernewton@hotmail.com>
2018-06-08 00:12:32 +0100

    Updated lots of modules

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
-
+app.use(express.static(path.join(__dirname + '.../public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 
 app.use(favicon(path.join(__dirname,'public/images/logo.ico')));
