d441fee334aa71dd48841d7bd188990a88615eff
Alex <xandernewton@hotmail.com>
2018-06-18 22:58:34 +0100

    Updated font colours

--- app/pythonScripts/eventPuller.py
+++ app/pythonScripts/eventPuller.py
@@ -5,15 +5,28 @@ from pprint import pprint
 from dotenv import load_dotenv
 import os
 import sys
+import time
 
 def main():
     
+    notComplete = True
+    tries = 0
     load_dotenv(os.path.join(os.path.dirname(os.getcwd()),'.env'))
   
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     db = DataBase(DB_USER,DB_PASSWORD)
-    EventsInfo = eventScraper()
+    while notComplete:
+        try:
+            EventsInfo = eventScraper()
+            notComplete = False
+        except Exception as e :
+            print(e)
+            tries +=1
+            time.sleep(300)
+            if tries == 3:
+                quit()
+
     db.checkForEventUpdate(EventsInfo)
 
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,19 +5,21 @@ const util = require('util')
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  MongoClient.connect(util.format('mongodb://%s:%s@ds263660.mlab.com:63660/events_db',process.env.DB_USER,process.env.DB_PASSWORD), (err, database) => {
+router.get('/', function (req, res) {
+  MongoClient.connect(util.format('mongodb://%s:%s@ds263660.mlab.com:63660/events_db', process.env.DB_USER, process.env.DB_PASSWORD), (err, database) => {
     if (err) return console.log(err);
     var db = database.db("events_db")
     var event = db.collection('events')
 
     var results = event.find({}).toArray(function (err, results) {
 
-      res.render('index',{'events' : results} );
+      res.render('index', {
+        'events': results
+      });
 
     })
 
-});
+  });
 
 });
 
