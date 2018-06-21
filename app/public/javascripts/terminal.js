d67a6e047ce78a2dc8cd9b2ef0e9d6e6cfa7e619
Alex <xandernewton@hotmail.com>
2018-06-21 14:21:16 +0100

    Added more content

---
+++ app/public/javascripts/terminal.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    
+
+    $('#terminal').typed({
+        strings: ["","We’re a dedicated bunch of enthusiasts and learners", 
+        "with a range of fun activities for beginners and veterans of cyber-security alike."],
+        typeSpeed: 50,
+        backDelay: 500,
+        smartBackspace : true,
+      
+      });
+    
+});
