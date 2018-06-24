fd822d5414e8c7a50458a7821ad0d94b87efdb1c
Alex <xandernewton@hotmail.com>
2018-06-24 21:36:45 +0100

    Added info to CTF page

--- app/public/javascripts/terminal.js
+++ app/public/javascripts/terminal.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     
 
     $('#terminal').typed({
-        strings: ["","We’re a dedicated bunch of enthusiasts and learners", 
-        "with a range of fun activities for beginners and veterans of cyber-security alike."],
+        strings: ["> We’re a dedicated bunch of enthusiasts and learners", 
+        "> with a range of fun activities for beginners and veterans of cyber-security alike."],
         typeSpeed: 50,
         backDelay: 500,
         smartBackspace : true,
