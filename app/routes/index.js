df0e1ecf9f3fd67a020c4e63af08fb1f712b7db6
Alex Newton <xandernewton@hotmail.com>
2018-09-12 22:20:08 +0100

    Events now split into upcoming and past, events ordered by date

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,14 +9,19 @@ router.get('/', function (req, res) {
   MongoClient.connect(util.format('mongodb://%s:%s@ds263660.mlab.com:63660/events_db', process.env.DB_USER, process.env.DB_PASSWORD), (err, database) => {
     if (err) return console.log(err);
     var db = database.db("events_db")
-    var event = db.collection('events')
-
-    var results = event.find({}).toArray(function (err, results) {
+    var event = db.collection('events_test')
+    var pastResults = event.find({event_Type : 'past'}).sort({event_StartTime: -1}).toArray()
+    var upcomingResults = event.find({event_Type : 'upcoming'}).sort({event_StartTime: -1}).toArray()
+    Promise.all([pastResults, upcomingResults]).then(function(values){
+      
+      let pastResults = values[0]
+      let upcomingResults = values[1]
 
       res.render('index', {
-        'previousEvents': results,
-        'currentEvents' : []
+        'previousEvents': pastResults,
+        'currentEvents' : upcomingResults
       })
+
 
     })
 
