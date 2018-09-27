bff1cc8e734c0eb63f4222f74ff6457897931840
Alex Newton <xandernewton@hotmail.com>
2018-09-27 14:31:50 +0100

    Magic number now re-directs to google drive

--- app/routes/workshops/freshers/magicnumber.js
+++ app/routes/workshops/freshers/magicnumber.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET about page. */
 router.get('/', function(req, res) {
 
-  res.download('./resources/magicnumber');
+  res.redirect('https://drive.google.com/open?id=1IjcdrxYNUu1Y7m3Gtm7GZxMTpf3ZxFvQ');
 });
 
 module.exports = router;
