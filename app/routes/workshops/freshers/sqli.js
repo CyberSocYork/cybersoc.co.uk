c939efa3f80d9582f4281f6eb2bcd47361dabc6a
Asher <asher@nas10-241-22-66.york.ac.uk>
2018-10-03 13:45:47 +0100

    Fixed incorrect link to SQLi presentation

--- app/routes/workshops/freshers/sqli.js
+++ app/routes/workshops/freshers/sqli.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET about page. */
 router.get('/', function(req, res) {
 
-  res.redirect('https://docs.google.com/presentation/d/14JpoCyUDuzEBeYesttZGsH0h6GMY68cpwZOKkMB6tPU/edit?usp=sharing');
+  res.redirect('https://docs.google.com/presentation/d/1BjGXKt75w-hny9zXSTwyaSlzuQUmdyIdxasmOoTel3Q/edit?usp=sharing');
 });
 
 module.exports = router;
