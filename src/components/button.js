37622973dbf9251798cddffedbb8df22fea63bf0
Ben Silverman <bensilverman10@gmail.com>
2020-02-11 13:21:44 +0000

    GigaStyle

--- src/components/button.js
+++ src/components/button.js
@@ -2,4 +2,8 @@ import React from "react";
 import "./button.css";
 
 // Need a way of
-export default (props) => <a class={`btn ${props.className}`}>{props.children}</a>;
+export default (props) => (
+	<a class={`btn ${props.className}`} href={props.href}>
+		{props.children}
+	</a>
+);
