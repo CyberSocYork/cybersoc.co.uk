dc08849daba75edf581abe16a86e630ab5a2d415
Ben Silverman <bensilverman10@gmail.com>
2020-02-18 23:29:18 +0000

    Added event images to cards and styled sections with accent colour

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./card.css";
 
+import cardImg from "../../img/card_img.jpg";
+
 export default (props) => (
 	<div class="card">
-		<h2 class="card-title">{props.title ? props.title : "Title"}</h2>
-		<h3 class="card-detail">{props.detail ? props.detail : "Time and place"}</h3>
-		<p class="card-desc">{props.desc ? props.desc : "Description"}</p>
+		<img src={cardImg} class="card-img" />
+		<div class="card-body">
+			<h2 class="card-title">{props.title ? props.title : "Title"}</h2>
+			<h3 class="card-detail">{props.detail ? props.detail : "Time and place"}</h3>
+			<p class="card-desc">{props.desc ? props.desc : "Description"}</p>
+		</div>
 	</div>
 );
