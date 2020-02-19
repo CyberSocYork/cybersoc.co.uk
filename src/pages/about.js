76d09c6a8d8b2887ae66f54d653693a594bd6f29
Ben Silverman <bensilverman10@gmail.com>
2020-02-19 15:41:39 +0000

    Added commitee section.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 // Components
 import Navbar from "../components/navbar/navbar.js";
+import CommitteeCard from "../components/committeeCard/committeeCard.js";
+
+import "../styles/about/about.css";
 
 export default () => (
 	<div>
+		<Helmet>
+			<title>About / York CyberSoc</title>
+		</Helmet>
 		<Navbar />
 		<div className="container">
 			<div className="row my-5" id="about">
 				<div className="col">
 					<h1>About Us</h1>
+					<p>CyberSoc is the University of York's Cyber Security Society.</p>
 					<p>
-						We are the University of York's Cyber Security Society. The society was
-						started in December 2017 by a group of computer science undergraduates with
-						a passion for cyber security. Have you ever been curious about the secret
-						lives of hackers? Do you want to learn about how to secure or exploit
-						computers for fun and profit? CyberSoc is the place for you! We’re a
-						dedicated bunch of enthusiasts and learners, with a range of fun activities
-						for beginners and veterans of cyber security alike. Come along to one of our
-						workshops to learn how to properly secure your personal devices, disappear
-						from the internet, or even compete in hacking competitions!
+						The society was started in December 2017 by a group of computer science
+						undergraduates with a passion for cyber security. If you've ever been
+						curious about the secret lives of hackers, or wanted to learn about how to
+						secure or exploit computers for fun and profit then CyberSoc is the place
+						for you!
+					</p>
+					<p>
+						We’re a dedicated bunch of enthusiasts and learners, with a range of fun
+						activities for beginners and veterans of cyber security alike. Come along to
+						one of our workshops (weekly, on Wednesdays) to learn how to properly secure
+						your personal devices, disappear from the internet, or even compete in
+						hacking competitions!
 					</p>
 				</div>
 			</div>
@@ -32,7 +43,47 @@ export default () => (
 						society.
 					</p>
 					<div className="committee-grid">
-						{/* TODO: Fill will divs for each committee member. Need to style with CSS grid! Could also create components for committee members? */}
+						<CommitteeCard
+							name="Tom O'Neill"
+							role="President"
+							desc="Responsible for leading the society and chairing committee meetings."
+						/>
+						<CommitteeCard
+							name="Jamie Quigley"
+							role="Secretary"
+							desc="Minutes committee meetings, ensure society admin is in order, and that the committee are carrying out their roles"
+						/>
+						<CommitteeCard
+							name="David Miall"
+							role="Treasurer"
+							desc="Manages the societies finances, including buying of new hardware and ensuring that society membership is paid."
+						/>
+						<CommitteeCard
+							name="Joe Wrieden"
+							role="Events Officer"
+							desc="Coordinates with the president to put on events and plan the timetable of society workshops and socials."
+						/>
+						<CommitteeCard
+							name="Ben Silverman"
+							role="Press & Publicity Officer"
+							desc="Responsible for sending society emails, operating society social media and organising society merchandise."
+						/>
+						<CommitteeCard
+							name="Asim Poptani"
+							role="Sysadmin"
+							desc="Responsible for coordinating the society's equipment acquisitions with the treasurer and maintaining the website and other systems necessary for the smooth running of workshops."
+						/>
+						<CommitteeCard
+							name="Alex Harvey"
+							role="Outreach Officer"
+							desc="A non-CS student, responsible for expanding the reach of the society to other departments."
+						/>
+						<CommitteeCard name="Alex Cook" role="Officer without Portfolio" desc=" " />
+						<CommitteeCard
+							name="Andy Chapman"
+							role="Officer without Portfolio"
+							desc=" "
+						/>
 					</div>
 				</div>
 			</div>
