3660c5013f85b139110ee2d6bd27b7206274cb4c
Ben Silverman <bensilverman10@gmail.com>
2020-02-20 12:27:23 +0000

    Added footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import "./footer.css";
+import { FaTwitter, FaFacebookF, FaDiscord } from "react-icons/fa";
+
+export default () => (
+	<footer>
+		<div className="container">
+			<div className="row">
+				<div className="col-12 col-md-10">
+					<p>To be filled.</p>
+					<a
+						href="https://twitter.com/CyberSocYork"
+						class="footer-social"
+						target="_blank"
+						rel="noopener noreferrer">
+						<FaTwitter />
+					</a>
+					<a
+						href="https://facebook.com/CyberSocYork"
+						class="footer-social"
+						target="_blank"
+						rel="noopener noreferrer">
+						<FaFacebookF />
+					</a>
+					<a
+						href="https://bit.ly/CyberSocDiscord"
+						class="footer-social"
+						target="_blank"
+						rel="noopener noreferrer">
+						<FaDiscord />
+					</a>
+				</div>
+				<div className="col-12 col-md-2">
+					<ul className="footer-links">
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about/">About</Link>
+						</li>
+						<li>
+							<Link to="/contact/">Contact</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+);
