a88d3e5bcb6ab0818d26ebfab8cc8de644affba4
Ben Silverman <bensilverman10@gmail.com>
2020-02-21 15:34:06 +0000

    Added contact page

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,7 +3,12 @@ import "./button.css";
 
 // Need a way of
 export default (props) => (
-	<a class={`btn ${props.className}`} href={props.href} target={props.target ? props.target : ""}>
+	<a
+		class={`btn ${props.className}`}
+		href={props.href}
+		target={props.target ? props.target : ""}
+		rel={props.rel ? props.rel : ""}
+		onfocus={props.onfocus ? props.onfocus : ""}>
 		{props.children}
 	</a>
 );
