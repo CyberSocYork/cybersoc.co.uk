b26db27b8329d36d8ca472cfc457b494665ceb4a
Ben Silverman <bensilverman10@gmail.com>
2020-04-01 16:54:55 +0100

    formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,20 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 module.exports = {
-  siteMetadata: {
-    title: `CyberSoc`,
-    siteUrl: `https://www.cybersoc.co.uk`,
-    description: `The official website for CyberSoc, the University of York's Cyber Security Society.`,
-  },
-  plugins: [
-	  `gatsby-plugin-react-helmet`,
-	  {
-		  resolve: `gatsby-source-filesystem`,
-		  options: {
-			  name: `posts`,
-			  path: `${__dirname}/src/markdown-posts`
-		  }
-	  },
-	  `gatsby-transformer-remark`
-  ]
-}
+    siteMetadata: {
+        title: `CyberSoc`,
+        siteUrl: `https://www.cybersoc.co.uk`,
+        description: `The official website for CyberSoc, the University of York's Cyber Security Society.`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/markdown-posts`,
+            },
+        },
+        `gatsby-transformer-remark`,
+    ],
+};
