bd06123cc7c533081fba94549bd2ca47b2f464dd
Ben Silverman <bensilverman10@gmail.com>
2020-04-01 16:19:12 +0100

    Configured gatsby-node to source and render markdown files

---
+++ gatsby-node.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+exports.createPages = async ({actions, graphql}) => {
+    const {createPage} = actions;
+    
+    const blogPostTemplate = path.resolve("src/templates/blogTemplate.js");
+
+    const result = await graphql(`
+        {
+            allMarkdownRemark(
+                sort: {
+                    order: DESC,
+                    fiels: [frontmatter___date]
+                }
+                limit: 1000
+            ) {
+                edges {
+                    node {
+                        frontmatter {
+                            path
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    // Handle errors
+    if (result.errors) {
+        return console.log(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+        createPage({
+            path: node.frontmatter.path,
+            component: blogPostTemplate,
+        })
+    })
+}
