98458213670409c89ec184dbb6d2a8d19f69635a
Ben Silverman <bensilverman10@gmail.com>
2020-04-01 17:54:51 +0100

    Installed and configured prismjs and remark-prismjs

---
+++ gatsby-browser.js
@@ -0,0 +1,1 @@
+require("prismjs/themes/prism-tomorrow.css"); // Provides syntax highlighting for inline code and code blocks.
--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,22 @@ module.exports = {
                 path: `${__dirname}/src/markdown-posts`,
             },
         },
-        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: "language-",
+                            inlineCodeMarker: null,
+                            aliases: {
+                                js: "javascript",
+                            },
+                        },
+                    },
+                ],
+            },
+        },
     ],
 };
