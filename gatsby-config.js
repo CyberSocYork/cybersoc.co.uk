9544e11646090e0078d84aaca5d6e67005ab373d
Ben Silverman <bensilverman10@gmail.com>
2020-04-01 16:04:34 +0100

    Installed gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
-{
-	plugins: [`gatsby-plugin-react-helmet`];
+module.exports = {
+  siteMetadata: {
+    title: `CyberSoc`,
+    siteUrl: `https://www.cybersoc.co.uk`,
+    description: `The official website for CyberSoc, the University of York's Cyber Security Society.`,
+  },
+  plugins: [
+	  `gatsby-plugin-react-helmet`,
+  ]
 }
-
-module.exports = {};
