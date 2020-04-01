b288a2a4dd92d641a1205dd7871bef041d7f9889
Ben Silverman <bensilverman10@gmail.com>
2020-04-01 17:56:31 +0100

    Added post author and date

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,29 +1,39 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { graphql } from "gatsby"
-import Navbar from "../components/navbar/navbar"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql } from "gatsby";
+import Navbar from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+
+import "../styles/blogTemplate/blogTemplate.css";
+
+// require("prismjs/themes/prism-solarizedlight.css");
 
 export default function Template({
     data, // this prop will be injected by the GraphQL query below.
 }) {
-    const { markdownRemark } = data // data.markdownRemark holds your post data
-    const { frontmatter, html } = markdownRemark
+    const { markdownRemark } = data; // data.markdownRemark holds the post data
+    const { frontmatter, html } = markdownRemark; // frontmatter is all the meta-content in the blog post, html is the actual content
     return (
-        <div>
+        <div class="site">
             <Helmet>
                 <title>{`Blog / ${frontmatter.title}`}</title>
             </Helmet>
             <Navbar />
-
-            <div className="container">
-                <div className="blog-post">
-                    <h1>{frontmatter.title}</h1>
-                    <h2>{frontmatter.date}</h2>
-                    <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+            <div className="blog-post container content">
+                <div className="row my-5">
+                    <div class="col">
+                        <h1 className="post-title">{frontmatter.title}</h1>
+                        <h2 className="post-detail">
+                            <span class="post-author">{frontmatter.author}</span> -{" "}
+                            <span class="post-date">{frontmatter.date}</span>
+                        </h2>
+                        <div className="post-content" dangerouslySetInnerHTML={{ __html: html }} />
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
-    )
+    );
 }
 
 export const pageQuery = graphql`
@@ -34,7 +44,8 @@ export const pageQuery = graphql`
                 date(formatString: "MMMM DD, YYYY")
                 path
                 title
+                author
             }
         }
     }
-`
+`;
