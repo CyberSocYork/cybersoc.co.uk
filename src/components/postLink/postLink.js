13a5ec6981606bd0d974225c37bd49a66385038b
Ben Silverman <bensilverman10@gmail.com>
2020-04-02 12:31:56 +0100

    Created PostLink entry structure

--- src/components/postLink/postLink.js
+++ src/components/postLink/postLink.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import "./postLink.css";
+
 const PostLink = ({ post }) => (
     <div>
-        <Link to={post.frontmatter.path}>
-            {post.frontmatter.title} ({post.frontmatter.date})
-        </Link>
+        <p class="post-link-date">
+            {post.frontmatter.author} on {post.frontmatter.date}
+        </p>
+        <h3 class="post-link-title">
+            <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+        </h3>
+        <p>{post.excerpt}</p>
+        <hr />
     </div>
 );
 
