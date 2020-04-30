aa57e3009b049c4c98557cbebf9291b3a70171aa
Ben Silverman <bensilverman10@gmail.com>
2020-04-30 17:58:31 +0100

    Add Layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+// Custom Components
+import Navbar from "../navbar/navbar.js";
+import Footer from "../footer/footer.js";
+
+import "./layout.css";
+
+export default (props) => (
+    <div class="site">
+        <Helmet>
+            <title>{props.title} / CyberSoc York</title>
+            {/* Add SEO, og:graph and any other metadata stuff in here */}
+        </Helmet>
+        <Navbar />
+        <div className="content">{props.children}</div>
+        <Footer />
+    </div>
+);
