2e3fa37d334f6a9c89180223becadb83c1e49c6f
Jacob Turner <jacob11turner@gmail.com>
2020-10-02 21:53:46 +0100

    Added sanitization for HTML parsing

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactHtmlParser from 'react-html-parser'; 
+import ReactHtmlParser from "react-html-parser";
+import DOMPurify from "dompurify";
 import "../styles/components/card.scss";
 
 import cardImg from "../img/card_img.jpg";
 
 export default (props) => (
-	<div className="card">
-		<img src={cardImg} alt="" className="card-img" />
-		<div className="card-body">
-			<h2 className="card-title">{props.title ? props.title : "Title"}</h2>
-			<h3 className="card-detail">{props.detail ? props.detail : "Time and place"}</h3>
-			<p className="card-desc">{ReactHtmlParser(props.desc ? props.desc : "Description")}</p>
-		</div>
-	</div>
+    <div className="card">
+        <img src={cardImg} alt="" className="card-img" />
+        <div className="card-body">
+            <h2 className="card-title">{props.title ? props.title : "Title"}</h2>
+            <h3 className="card-detail">{props.detail ? props.detail : "Time and place"}</h3>
+            <p className="card-desc">
+                {ReactHtmlParser(DOMPurify.sanitize(props.desc ? props.desc : "Description"))}
+            </p>
+        </div>
+    </div>
 );
