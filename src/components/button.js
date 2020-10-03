8628ddd45b2a0df0ad421b106b2881e36414e92f
Ben Silverman <bensilverman10@gmail.com>
2020-10-03 18:33:47 +0100

    Enable buttons to be either Links or anchor tags

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Link } from "gatsby";
+
 import "../styles/components/button.scss";
 
-export default (props) => (
-	<a
-		className={`btn ${props.className}`}
-		href={props.href}
-		target={props.target ? props.target : ""}
-		rel="noopener noreferrer">
-		{props.children}
-	</a>
-);
+export default (props) => {
+    if (props.isLocal) {
+        return (
+            <Link
+                className={`btn ${props.className ? props.className : ""}`}
+                to={props.to}
+                target={props.target ? props.target : ""}
+                rel="noopener noreferrer">
+                {props.children}
+            </Link>
+        );
+    } else {
+        return (
+            <a
+                className={`btn ${props.className ? props.className : ""}`}
+                href={props.href}
+                target={props.target ? props.target : ""}
+                rel="noopener noreferrer">
+                {props.children}
+            </a>
+        );
+    }
+};
